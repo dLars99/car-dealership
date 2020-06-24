ed834689f25dc0a30b6703f43e9c3777c9fb5617
David Larsen <davidplarsen@gmail.com>
2020-06-23 21:42:30 -0500

    Highest monthly sales

--- cars.js
+++ cars.js
@@ -8,8 +8,30 @@ const profit2017 = salesDatabase.filter(record => {
 .reduce((cumulative, record) => cumulative + record.gross_profit, 0)
 .toFixed(2)
 
-answerPane.innerHTML = `Total profit for 2017: $${profit2017}`
+answerPane.innerHTML = `<p>Total profit for 2017: $${profit2017}</p>`
 
 // Answer to question 2: In which month did they sell the most cars?
+
+let highestMonth = {month: "", dollars: 0}
+const monthlySales = salesDatabase.map(record => {
+    const month = record.purchase_date.substr(0, 7)
+    return {month: month, dollars: record.gross_profit}
+}).reduce((acc, cur) => {
+    acc[cur.month] = acc[cur.month] + cur.dollars || cur.dollars;
+    return acc;
+}, {})
+
+console.log(monthlySales)
+
+for (const eachMonth in monthlySales) {
+    if (monthlySales[eachMonth] > highestMonth.dollars) {
+        highestMonth.month = eachMonth
+        highestMonth.dollars = monthlySales[eachMonth]
+    }
+}
+
+answerPane.innerHTML += `<p>Highest month of sales: ${highestMonth.month}</p>`
+
+// Sum all sales for each month
 
 
